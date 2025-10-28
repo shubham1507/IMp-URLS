1b8b4d021800cca239a63207e9f97f37c0ed1abd
shubham joshi <shubham.joshi1507@gmail.com>
2025-10-28 13:15:26 +0530

    Delete dashboard.tsx

--- dashboard.tsx
+++
@@ -1,5 +0,0 @@
-const handleRolePick = (rowId, roleId) => {
-  setTempRoleByRow((prev) => ({ ...prev, [rowId]: roleId }));
-  setDirtyRowIds((prev) => new Set(prev).add(rowId)); // mark as changed
-  closeRolePopover();
-};
